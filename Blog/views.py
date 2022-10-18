d7d50ccdda266f3117ac06f05b97f4be6fad197f
antonnifo <antonnifo@live.com>
2022-10-18 11:25:36 +0300

    add index view 4homepage

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.postgres.search import (SearchQuery, SearchRank,
+                                            SearchVector)
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
+from django.shortcuts import get_object_or_404, render
+from taggit.models import Tag
 
-# Create your views here.
+from hitcount.utils import get_hitcount_model
+from hitcount.views import HitCountMixin
+
+from .models import Category, Comment, Post
+
+
+OBJECT_LIST = Post.published.all()
+
+
+def index(request):
+ 
+    return render (request, 'site/index.html' ,{
+        'posts': OBJECT_LIST[:8]
+    })
+
+   
