e4749b8123ae858f7fa3105b8505804994657cc8
antonnifo <antonnifo@live.com>
2022-10-19 09:32:08 +0300

    add sidebar inclusion tag

--- Blog/templatetags/code_tags.py
+++ Blog/templatetags/code_tags.py
@@ -2,22 +2,25 @@ import markdown
 from django import template
 from django.utils.safestring import mark_safe
 from Blog.models import Post, Category
+from taggit.models import Tag
+
 
 
 register = template.Library()
+
+
+OBJECT_LIST = Post.published.all()
 
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
 
 
-# @register.inclusion_tag('blog/blog_sidebar.html')
-# def show_sidebar(count=5):
-#     popular_posts = Post.objects.all().order_by('-hit_count_generic__hits')[:3]
-#     return {
-#         'popular_posts' : popular_posts,
-#         'categories': Category.objects.order_by('-name')[:count],
-#         'tags': Post.tags.most_common()[:10],
-    
-    
-#     }
+@register.inclusion_tag('site/sidebar.html')
+def show_sidebar(count=5):
+    return {
+            'tags' : Post.tags.most_common()[:10],
+            'featured' : OBJECT_LIST.filter(featured=True),
+            'categories' : Category.objects.all()[:count],
+            'popular_posts' : OBJECT_LIST.order_by('-hit_count_generic__hits')[:3]          
+    }
