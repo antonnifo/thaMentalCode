37bbdd95ac5c438ce2fab5955c37b53f235492b0
antonnifo <antonnifo@live.com>
2022-10-19 10:44:00 +0300

    map post_list views

--- Blog/urls.py
+++ Blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
      path('', views.index, name='home'),
      path('<int:year>/<int:month>/<int:day>/<slug:post>/',
                 views.post_detail, name='post_detail'),
+     path('tag/<slug:tag_slug>/',
+            views.list_category, name='post_list_by_tag'),   
+     path('category/<slug:category_slug>/',
+                      views.list_category, name='post_list_by_category'),                
 ]
